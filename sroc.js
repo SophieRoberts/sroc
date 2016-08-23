5fa4c25bdcdd71bda195825e38976b4160edece4
Sophie Roberts <sophie@spanner.org>
2016-08-23 16:30:22 +0100

    first website

---
+++ sroc.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+	$("#event1").load("fixtures.html #fixture1");
+	$("#event2").load("fixtures.html #fixture2");
+	$("#event3").load("fixtures.html #fixture3");
+	$("#recent1").load("results.html #result1");
+	$("#recent2").load("results.html #result2");
+	$("#recent3").load("results.html #result3");
+	$("#recent4").load("results.html #result4");
+	$("#recent5").load("results.html #result5");
+})
